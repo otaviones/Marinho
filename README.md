0d64423a0600fc28c641a8ba248dbd0ea62cb7b8
otaviones <azeredo50@gmail.com>
2025-11-24 21:55:42 -0300

    first deploy for CI/CD

---
+++ README.md
@@ -0,0 +1,4 @@
+# Construtora marinho
+## Construindo Sonhos, Construindo Lares!
+### Tudo por você e sua família.
+
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configurando o EJS como motor de visualização
 app.set('view engine', 'ejs');
@@ -15,10 +15,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     // Aqui futuramente buscaremos dados do banco para enviar para a view
     // Por enquanto, vamos apenas renderizar a home
-    res.render('index');
+    const projetos = [
+        {
+            id: 0,
+            titulo: "Casa 0",
+            descriao: "Essa é a descricao da Casa 0.",
+            imagem: "/img/FACHADA0.png"
+        },
+        {
+            id: 1,
+            titulo: "Casa 1",
+            descriao: "Essa é a descricao da Casa 1.",
+            imagem: "/img/FACHADA1.png"
+        },
+        {
+            id: 2,
+            titulo: "Casa 2",
+            descriao: "Essa é a descricao da Casa 2.",
+            imagem: "/img/FACHADA2.png"
+        },
+        {
+            id: 3,
+            titulo: "Casa 3",
+            descriao: "Essa é a descricao da Casa 3.",
+            imagem: "/img/FACHADA3.png"
+        },
+        {
+            id: 4,
+            titulo: "Casa 4",
+            descriao: "Essa é a descricao da Casa 4.",
+            imagem: "/img/FACHADA4.png"
+        },
+    ];
+
+    res.render('index', { projetos: projetos });
 });
 
 // Iniciando o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando na porta${port}`);
 });
